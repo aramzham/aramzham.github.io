b4a620b230b42f8b2236b19218e86572efa9c972
Aram Zhamkochyan <aramzham@hotmail.com>
2023-07-07 12:35:35 +0200

    Create hackedTextEffect.js

---
+++ js/hackedTextEffect.js
@@ -0,0 +1,32 @@
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+const allPs = document.querySelectorAll("#contact .contact-info .contact-item span:not(.icon > span)");
+
+allPs.forEach((element) => {
+	let interval = null;
+	
+	element.onmouseover = event => {  
+	let iteration = 0;
+
+	clearInterval(interval);
+
+	interval = setInterval(() => {
+			event.target.innerText = event.target.innerText
+			  .split("")
+			  .map((letter, index) => {
+				if(index < iteration) {
+				  return event.target.dataset.value[index];
+				}
+			  
+				return letters[Math.floor(Math.random() * 26)]
+			  })
+			  .join("");
+
+			if(iteration >= event.target.dataset.value.length){ 
+			  clearInterval(interval);
+			}
+
+			iteration += 1 / 3;
+		}, 30);
+	}
+});
